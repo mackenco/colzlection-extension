eba2730031ce9dc23d17c68e0a8d17f55c358a95
Colin <colinwmackenzie@gmail.com>
2016-02-24 23:48:37 -0500

    delete

--- popup.js
+++ popup.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
   var addPageButton = document.getElementById('addPage');
+  var oopsButton = document.getElementById('oops');
+  var d = document;
 
   addPageButton.addEventListener('click', function() {
 
     var cb = document.getElementById('finished');
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
-      var url = tabs[0].url;
+
+      var f = d.createElement('form');
+      f.action = 'https://agile-chamber-71691.herokuapp.com/';
+      f.method = 'post';
+
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tabs[0].url;
+      f.appendChild(i);
+      
+      var i2 = d.createElement('input');
+      i2.type = 'hidden';
+      i2.name = 'finished';
+      i2.value = cb.checked;
+      f.appendChild(i2);
+
+      d.body.appendChild(f);
+      f.submit();
     });
   }, false);
+
+  oopsButton.addEventListener('click', function() {
+    var f = d.createElement('form');
+    f.action = 'https://agile-chamber-71691.herokuapp.com/?pop=true';
+    f.method = 'post';
+
+    d.body.appendChild(f);
+    f.submit();
+  }, false);
+
 }, false);
